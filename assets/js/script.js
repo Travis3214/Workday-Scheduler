d06859efd38a3ae04aa08b5bd11c75821fc992ca
Travis <travis3214@yahoo.com>
2023-07-10 20:08:55 -0400

    added 12pm-5pm in html and tested day js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+const currentTime =  dayjs();
+console.log(currentTime.format("MM/DD/YYYY h:mm A"));
+
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+
+  
+});
+
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,4 +28,3 @@ $(document).ready(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
